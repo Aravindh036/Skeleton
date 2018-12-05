2dbbc6d55a7306c9a0ebd14402dc778f1754e82e
Aravindh036 <aravindh036@gmail.com>
2018-12-05 11:19:41 +0530

    temp

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ var add = require('./add');
 var sub = require('./sub');
 var div = require('./div');
 var mul = require('./mul');
-window.hellofun = function hellofun(){
-    var num1 = document.getElementById('num1').value - '0';
-    var num2 = document.getElementById('num2').value - '0';
-    if(num1==null||num2==null){
-        alert("enter the values");
-    }else{
-        var operation = document.getElementById('select-id').value;
+window.hellofun = function hellofun(var1,var2,operation){
+    // var num1 = document.getElementById('num1').value - '0';
+    // var num2 = document.getElementById('num2').value - '0';
+    // if(num1==null||num2==null){
+    //     alert("enter the values");
+    // }else{
+    //     var operation = document.getElementById('select-id').value;
+    this.console.log(operation);
         if(operation=="add"){
-            execute(num1,num2,add);
+            execute(var1,var2,add);
         }
         else if(operation=="sub"){
-            execute(num1,num2,sub);
+            execute(var1,var2,sub);
         }
         else if(operation=="div"){
-            execute(num1,num2,div);
+            execute(var1,var2,div);
         }
         else{
-            execute(num1,num2,mul);
+            execute(var1,var2,mul);
         }
-    }
+    // }
 }
 
 function execute(num1,num2,callback){
